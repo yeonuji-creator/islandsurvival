5ec5274137610bd98359c4331ec91270d72dea41
Juhee Park <58504730+Juhee-Park@users.noreply.github.com>
2026-08-29 21:30:59 +0900

    Uncomment music loading and playing in onebyone

--- main.py
+++ main.py
@@ -29,9 +29,9 @@ inventory = {
 def onebyone(sentence, s_end = '\n'):
     for i in sentence:
         print(i, end="", flush = True)
-        #pygame.mixer.music.load("audio/type.mp3")
+        pygame.mixer.music.load("audio/type.mp3")
 
-        #pygame.mixer.music.play()
+        pygame.mixer.music.play()
         time.sleep(0.3)
         
 
