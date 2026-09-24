546393b4b9cf1f08212517d9f7c0cbe47822940e
yeonuji <yeonuji270@gmail.com>
2026-09-24 09:12:33 +0900

    Increase wood requirement for boat construction
    Updated wood requirement for boat construction from 3 to 10.

--- win.py
+++ win.py
@@ -13,10 +13,10 @@ inventory = {
     }
 
 onebyone("당신은 60일차까지 살아남았습니다.")
-if inventory['목재']>=3:
+if inventory['목재'] >= 10:
     onebyone("당신은 남은 목재로 배를 만들고 떠났습니다.")
     onebyone("---끝---")
 else:
-    onebyone("당신은 목재가 부족합니다.")
+    onebyone("당신은 목재가 부족합니다.(필요한 목재: 10개)")
     onebyone("목재를 더 모아서 배를 만드시오.")
 
